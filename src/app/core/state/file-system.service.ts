f1b450d19ed79960a45644a96bf94e93de8fb4fc
Davi Barreto <davivbarreto@gmail.com>
2020-08-22 02:45:53 +0300

    Add edit metadata logic

--- src/app/core/state/file-system.service.ts
+++ src/app/core/state/file-system.service.ts
@@ -7,34 +7,53 @@ import { HashService } from '../services/hash.service';
 import { DreeWithMetadata } from '../types';
 import { Repository } from 'typeorm';
 import { Metadata } from '../db/entities/metadata.entity';
+import { Dree } from 'dree';
 
 @Injectable({ providedIn: 'root' })
 export class FileSystemService {
+  private metadataRepo: Promise<Repository<Metadata>>;
   constructor(
     private fileSystemStore: FileSystemStore,
     private hashService: HashService,
     private dbService: DatabaseService
   ) {
+    this.metadataRepo = this.dbService.connection.then((c) =>
+      c.getRepository(Metadata)
+    );
+
     this.navigate();
   }
 
-  private async getDreeWithHash(folder: string) {
+  private async getDreeWithHashAndMeta(
+    folder: string
+  ): Promise<DreeWithMetadata> {
+    const metadataRepo = await this.metadataRepo;
+
     const dreeScan = dree.scan(folder, { hash: false, depth: 1 });
-    const children = dreeScan.children || [];
+    const children: DreeWithMetadata[] = dreeScan.children || [];
     for (let i = 0; i < children.length; i++) {
       const child = children[i];
       if (child.type === 'file') {
         child.hash = await this.hashService.hashFile(child.path);
+        child.metadata = await this.loadMetadata(child, metadataRepo);
       }
     }
 
     return dreeScan;
   }
 
+  private async loadMetadata(
+    node: Dree,
+    metadataRepo: Repository<Metadata>
+  ): Promise<Metadata> {
+    return metadataRepo.findOne({
+      hash: node.hash,
+      sizeInBytes: node.sizeInBytes,
+    });
+  }
+
   public async addMetadata(node: DreeWithMetadata, metadataContent: string) {
-    const metadataRepo = (await this.dbService.connection).getRepository(
-      Metadata
-    );
+    const metadataRepo = await this.metadataRepo;
     await metadataRepo.save({
       hash: node.hash,
       content: metadataContent,
@@ -47,7 +66,7 @@ export class FileSystemService {
   public async navigate(
     folder: string = this.fileSystemStore.getValue().folderPath
   ) {
-    const dreeScan = await this.getDreeWithHash(folder);
+    const dreeScan = await this.getDreeWithHashAndMeta(folder);
 
     this.fileSystemStore.update((state) => ({
       folderPath: folder,
