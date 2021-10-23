16e0b064b1c06685e830d4ec4c876b0dc6d2a141
Davi Barreto <davivbarreto@gmail.com>
2021-10-23 19:06:45 +0300

    fix: translation not working in windows prod build

--- src/app/transloco-root.module.ts
+++ src/app/transloco-root.module.ts
@@ -1,26 +1,32 @@
 import { HttpClient } from '@angular/common/http';
-import {
-  TRANSLOCO_LOADER,
-  Translation,
-  TranslocoLoader,
-  TRANSLOCO_CONFIG,
-  translocoConfig,
-  TranslocoModule
-} from '@ngneat/transloco';
 import { Injectable, NgModule } from '@angular/core';
+import {
+  Translation,
+  translocoConfig,
+  TranslocoLoader,
+  TranslocoModule,
+  TRANSLOCO_CONFIG,
+  TRANSLOCO_LOADER,
+} from '@ngneat/transloco';
+import * as url from 'url';
+import * as path from 'path';
 import { environment } from '../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
   constructor(private http: HttpClient) {}
-
   getTranslation(lang: string) {
-    return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
+    const fileUrl = url.format({
+      pathname: path.join(__dirname, 'assets', 'i18n', `/${lang}.json`),
+      protocol: 'file:',
+      slashes: true,
+    });
+    return this.http.get<Translation>(fileUrl);
   }
 }
 
 @NgModule({
-  exports: [ TranslocoModule ],
+  exports: [TranslocoModule],
   providers: [
     {
       provide: TRANSLOCO_CONFIG,
@@ -30,9 +36,9 @@ export class TranslocoHttpLoader implements TranslocoLoader {
         // Remove this option if your application doesn't support changing language in runtime.
         reRenderOnLangChange: true,
         prodMode: environment.production,
-      })
+      }),
     },
-    { provide: TRANSLOCO_LOADER, useClass: TranslocoHttpLoader }
-  ]
+    { provide: TRANSLOCO_LOADER, useClass: TranslocoHttpLoader },
+  ],
 })
 export class TranslocoRootModule {}
